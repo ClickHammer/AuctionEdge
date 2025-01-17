1f654f1afe11b09437f44cefe535cca6f5928c28
Ujjwalgupt96 <guptaujjwal944@gmail.com>
2025-01-18 02:25:09 +0530

    some jwt errors rectified

--- Backend/utils/jwtTokens.js
+++ Backend/utils/jwtTokens.js
@@ -2,7 +2,7 @@ export const generateToken=(user,message,statusCode,res)=>{
     const token =user.getJwtToken();
     res.status(statusCode).
     cookie("token",token,{
-        expires :new Date(Date.now() + process.env.JWT_EXPIRES_TIME * 24 * 60 * 60 * 1000
+        expires :new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly:true})
     .json({
