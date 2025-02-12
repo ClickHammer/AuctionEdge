c7a3290b5804ddee83cd96b3689f2d9b0ca15f51
sparsh2467 <guptasparsh1406@gmail.com>
2025-02-13 00:26:06 +0530

    Frontend Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SideDrawer from "./layout/SideDrawer";
+import Home from "./pages/Home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUser());
+    dispatch(getAllAuctionItems());
+    dispatch(fetchLeaderboard());
+  }, []);
+  return (
+    <Router>
+      <SideDrawer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/submit-commission" element={<SubmitCommission />} />
+        <Route path="/how-it-works-info" element={<HowItWorks />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/auctions" element={<Auctions />} />
+        <Route path="/auction/item/:id" element={<AuctionItem />} />
+        <Route path="/create-auction" element={<CreateAuction />} />
+        <Route path="/view-my-auctions" element={<ViewMyAuctions />} />
+        <Route path="/auction/details/:id" element={<ViewAuctionDetails />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/me" element={<UserProfile />} />
+      </Routes>
+      <ToastContainer position="top-right" />
+    </Router>
+  );
+};
+export default App
