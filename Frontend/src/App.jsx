5cfe860408a9adc771133b469b653fb6434a3722
sparsh2467 <guptasparsh1406@gmail.com>
2025-03-25 22:20:59 +0530

    SideDrawer Completed Successfully

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import  { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import SideDrawer from "./layout/SideDrawer";
+import SideDrawer from "./layout/SideDrawer";
 import Home from "./pages/Home.jsx";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
   
   return (
     <div>
       <Router>
-      {/* { <SideDrawer /> } */}
+      { <SideDrawer /> }
       <Routes>
       <Route path='/' element={<Home/>} />
       </Routes>
       </Router>
-      {/* { <ToastContainer position="top-right" /> } */}
+      { <ToastContainer position="top-right" /> }
       </div>
   );
 };
