5f9d3630465ac452d3849b703e09f7349d3b60b4
Ujjwalgupt96 <guptaujjwal944@gmail.com>
2025-03-29 16:50:30 +0530

    Some changes in Ui

--- Frontend/src/layout/sidedrawer.jsx
+++ Frontend/src/layout/sidedrawer.jsx
@@ -3,51 +3,51 @@ import { RiAuctionFill } from "react-icons/ri";
 import { MdLeaderboard, MdDashboard } from "react-icons/md";
 import { SiGooglesearchconsole } from "react-icons/si";
 import { BsFillInfoSquareFill } from "react-icons/bs";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaUserCircle, FaFileInvoiceDollar, FaEye } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdCloseCircleOutline, IoIosCreate } from "react-icons/io";
-import { FaUserCircle } from "react-icons/fa";
-import { FaFileInvoiceDollar } from "react-icons/fa6";
-import { FaEye } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "@/store/slices/UserSlice.js";
 import { Link } from "react-router-dom";
 
 const SideDrawer = () => {
   const [show, setShow] = useState(false);
-
   const { isAuthenticated, user } = useSelector((state) => state.user);
-
   const dispatch = useDispatch();
+
   const handleLogout = () => {
     dispatch(logout());
   };
 
   return (
     <>
+      {/* Hamburger Icon */}
       <div
         onClick={() => setShow(!show)}
-        className="fixed right-5 top-5 bg-[#D6482B] text-white text-3xl p-2 rounded-md hover:bg-[#b8381e] lg:hidden"
+        className="fixed right-5 top-5 bg-[#15317E] text-white text-3xl p-2 rounded-md lg:hidden"
       >
         <GiHamburgerMenu />
       </div>
+
+      {/* Side Drawer */}
       <div
-        className={`w-[100%] sm:w-[300px] bg-[#ffffff] h-full fixed top-0 ${
+        className={`w-[100%] sm:w-[300px] bg-white h-full fixed top-0 ${
           show ? "left-0" : "left-[-100%]"
-        } transition-all duration-100 p-4 flex flex-col justify-between lg:left-0 border-r-[1px] border-r-stone-500`}
+        } transition-all duration-200 p-4 flex flex-col justify-between lg:left-0 border-r-[1px] border-gray-300`}
       >
         <div className="relative">
           <Link to={"/"}>
             <h4 className="text-2xl font-semibold mb-4">
-              <span className="text-[#D6482b]">AuctionEdge</span>
+              <span className="text-[#15317E]">AuctionEdge</span>
             </h4>
           </Link>
+
           <ul className="flex flex-col gap-3">
             <li>
               <Link
                 to={"/auctions"}
-                className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                className="flex text-xl font-semibold gap-2 items-center hover:text-[#7D6115] transition duration-150"
               >
                 <RiAuctionFill /> Auctions
               </Link>
@@ -55,17 +55,18 @@ const SideDrawer = () => {
             <li>
               <Link
                 to={"/leaderboard"}
-                className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                className="flex text-xl font-semibold gap-2 items-center hover:text-[#7D6115] transition duration-150"
               >
                 <MdLeaderboard /> Leaderboard
               </Link>
             </li>
+
             {isAuthenticated && user && user.role === "Auctioneer" && (
               <>
                 <li>
                   <Link
                     to={"/submit-commission"}
-                    className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                    className="flex text-xl font-semibold gap-2 items-center hover:text-[ #7D6115] transition duration-150"
                   >
                     <FaFileInvoiceDollar /> Submit Commission
                   </Link>
@@ -73,7 +74,7 @@ const SideDrawer = () => {
                 <li>
                   <Link
                     to={"/create-auction"}
-                    className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                    className="flex text-xl font-semibold gap-2 items-center hover:text-[ #7D6115] transition duration-150"
                   >
                     <IoIosCreate /> Create Auction
                   </Link>
@@ -81,57 +82,60 @@ const SideDrawer = () => {
                 <li>
                   <Link
                     to={"/view-my-auctions"}
-                    className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                    className="flex text-xl font-semibold gap-2 items-center hover:text-[ #7D6115] transition duration-150"
                   >
                     <FaEye /> View My Auctions
                   </Link>
                 </li>
               </>
             )}
+
             {isAuthenticated && user && user.role === "Super Admin" && (
               <li>
                 <Link
                   to={"/dashboard"}
-                  className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                  className="flex text-xl font-semibold gap-2 items-center hover:text-[ #7D6115] transition duration-150"
                 >
                   <MdDashboard /> Dashboard
                 </Link>
               </li>
             )}
           </ul>
+
           {!isAuthenticated ? (
-            <>
-              <div className="my-4 flex gap-2">
-                <Link
-                  to={"/sign-up"}
-                  className="bg-[#D6482B] font-semibold hover:bg-[#b8381e] text-xl py-1 px-4 rounded-md text-white"
-                >
-                  Sign Up
-                </Link>
-                <Link
-                  to={"/login"}
-                  className="text-[#ffffff] bg-transparent border-[#DECCBE] border-2 hover:bg-[#fffefd] hover:text-[#fdba88] font-bold text-xl py-1 px-4 rounded-md"
-                >
-                  Login
-                </Link>
-              </div>
-            </>
+            <div className="my-4 flex gap-2">
+              <Link
+  to={"/sign-up"}
+  className="bg-[#15317E] border-2 border-transparent font-semibold text-xl py-1 px-4 rounded-md text-white 
+             hover:bg-white hover:border-[#15317E] hover:text-[#15317E] transition duration-200"
+>
+  Sign Up
+</Link>
+<Link
+  to={"/login"}
+  className="border-2 border-[#15317E] text-[#15317E] font-semibold text-xl py-1 px-4 rounded-md 
+             hover:bg-[#15317E] hover:text-white transition duration-200"
+>
+  Login
+</Link>
+
+            </div>
           ) : (
-            <>
-              <div className="my-4 flex gap-4 w-fit" onClick={handleLogout}>
-                <button className="bg-[#D6482B] font-semibold hover:bg-[#b8381e] text-xl py-1 px-4 rounded-md text-white">
-                  Logout
-                </button>
-              </div>
-            </>
+            <div className="my-4 flex gap-4 w-fit" onClick={handleLogout}>
+              <button className="bg-[#15317E] font-semibold text-xl py-1 px-4 rounded-md text-white hover:bg-[#E67E22] transition duration-200">
+                Logout
+              </button>
+            </div>
           )}
-          <hr className="mb-4 border-t-[#d6482b]" />
+
+          <hr className="mb-4 border-t-[#15317E]" />
+
           <ul className="flex flex-col gap-3">
             {isAuthenticated && (
               <li>
                 <Link
                   to={"/me"}
-                  className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                  className="flex text-xl font-semibold gap-2 items-center hover:text-[#7D6115] transition duration-150"
                 >
                   <FaUserCircle /> Profile
                 </Link>
@@ -140,7 +144,7 @@ const SideDrawer = () => {
             <li>
               <Link
                 to={"/how-it-works-info"}
-                className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                className="flex text-xl font-semibold gap-2 items-center hover:text-[#7D6115] transition duration-150"
               >
                 <SiGooglesearchconsole /> How it works
               </Link>
@@ -148,15 +152,16 @@ const SideDrawer = () => {
             <li>
               <Link
                 to={"/about"}
-                className="flex text-xl font-semibold gap-2 items-center hover:text-[#D6482b] hover:transition-all hover:duration-150"
+                className="flex text-xl font-semibold gap-2 items-center hover:text-[#7D6115] transition duration-150"
               >
                 <BsFillInfoSquareFill /> About Us
               </Link>
             </li>
           </ul>
+
           <IoMdCloseCircleOutline
             onClick={() => setShow(!show)}
-            className="absolute top-0 right-4 text-[28px] sm:hidden"
+            className="absolute top-0 right-4 text-[28px] sm:hidden text-gray-500 hover:text-[#7D6115]"
           />
         </div>
 
@@ -164,31 +169,31 @@ const SideDrawer = () => {
           <div className="flex gap-2 items-center mb-2">
             <Link
               to="/"
-              className="bg-white text-stone-500 p-2 text-xl rounded-sm hover:text-blue-700"
+              className="bg-white text-gray-500 p-2 text-xl rounded-sm hover:text-blue-700"
             >
               <FaFacebook />
             </Link>
             <Link
               to="/"
-              className="bg-white text-stone-500 p-2 text-xl rounded-sm hover:text-pink-500"
+              className="bg-white text-gray-500 p-2 text-xl rounded-sm hover:text-pink-500"
             >
               <RiInstagramFill />
             </Link>
           </div>
           <Link
             to={"/contact"}
-            className="text-stone-500 font-semibold hover:text-[#d6482b] hover:transition-all hover:duration-150"
+            className="text-gray-500 font-semibold hover:text- [#7D6115] transition duration-150"
           >
             Contact Us
           </Link>
-          <p className="text-stone-500">&copy; AuctionEdge, LLC.</p>
-          <p className="text-stone-500">
-            Desgined By{" "}
+          <p className="text-gray-500">&copy; AuctionEdge, LLC.</p>
+          <p className="text-gray-500">
+            Designed By{" "}
             <Link
               to={"/"}
-              className="font-semibold hover:text-[#d6482b] hover:transition-all hover:duration-150"
+              className="font-semibold hover:text-[#7D6115] transition duration-150"
             >
-              Sparsh Gupta
+              AuctionEdge & Team
             </Link>
           </p>
         </div>
