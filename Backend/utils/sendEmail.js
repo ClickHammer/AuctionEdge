fcbfda723ac3ff9016018f6774af86e8ac7752e5
Ujjwal-Guptaa296 <guptaujjwal934@gmail.com>
2026-01-17 17:50:20 +0530

    nodemaier issue correction

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,6 +1,6 @@
 import nodeMailer from "nodemailer";
   export const sendEmail = async ({ email, subject, message }) => {
-  const transporter = nodemailer.createTransport({
+  const transporter = nodeMailer.createTransport({
    host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     secure: true,
