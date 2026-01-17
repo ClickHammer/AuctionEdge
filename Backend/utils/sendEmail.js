4f100c573dbf74f51ae663903bbdee535c107380
Ujjwal-Guptaa296 <guptaujjwal934@gmail.com>
2026-01-17 17:34:09 +0530

    smtp production level setup

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,13 +1,12 @@
 import nodeMailer from "nodemailer";
-
-export const sendEmail = async ({ email, subject, message }) => {
-  const transporter = nodeMailer.createTransport({
-    host: process.env.SMTP_HOST,
+  export const sendEmail = async ({ email, subject, message }) => {
+  const transporter = nodemailer.createTransport({
+   host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    service: process.env.SMTP_SERVICE,
+    secure: true,
     auth: {
       user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
+      pass: process.env.SMTP_PASSWORD, // App Password
     },
   });
   const options = {
